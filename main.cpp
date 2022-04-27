c0e8c9a91681d90f0d318e34734d534cae618df1
LanceloT7730 <ac2952602@akfauniversity.org>
2022-04-27 20:10:28 +0500

    statements are completed for SE department

--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
 using namespace std;
 
 vector<string> reader(string file_name);
+void cases ();
 
 int main() {
-    vector<string> data = reader("timetable.txt");
+
+    cases();
 
     return 0;
 }
 
 vector<string> reader(string file_name) {
     string search_term;
-    getline(cin, search_term);
+    cout << "Enter the day you want to see: " << flush;
+    cin >> search_term;
     vector<string> record;
     ifstream file;
     file.open(file_name);
@@ -38,7 +40,18 @@ vector<string> reader(string file_name) {
         }
     }
     for (auto i: record) {
-        cout << i << " ";
+        cout << i << '\n';
     }
+
     return record;
+}
+
+void cases() {
+    int group_number{0};
+    cout << "Enter your group number: ";
+    cin >> group_number;
+    if (group_number == 1) vector<string> se2101 = reader("timetable_se2101.txt");
+    else if (group_number == 2) vector<string> se2102 = reader("timetable_se2102.txt");
+    else if (group_number == 3) vector<string> se2103 = reader("timetable_se2103.txt");
+    else if (group_number == 4) vector<string> se2104 = reader("timetable_se2104.txt");
 }
