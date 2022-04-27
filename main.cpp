e4f792df8e58c6b99c5e5efcc25acc10e576bc4c
LanceloT7730 <ac2952602@akfauniversity.org>
2022-04-27 18:21:51 +0500

    The project started

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> reader(string file_name);
+
+int main() {
+    vector<string> data = reader("timetable.txt");
+
+    return 0;
+}
+
+vector<string> reader(string file_name) {
+    string search_term;
+    string search;
+    getline(cin, search);
+    search_term = search;
+    vector<string> record;
+    ifstream file;
+    file.open(file_name);
+
+    bool found_record = false;
+
+    string field_one;
+    string field_two;
+    string field_three;
+
+    while (getline(file, field_one, ',') && !found_record) {
+        getline(file, field_two, ',');
+        getline(file, field_three, '\n');
+        if (field_one == search_term) {
+            found_record = true;
+            record.push_back(field_one);
+            record.push_back(field_two);
+
+            record.push_back(field_three);
+        }
+    }
+    for (auto i: record) {
+        cout << i << " ";
+    }
+    return record;
+}
