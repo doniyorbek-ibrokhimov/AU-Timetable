a11bd45de89931dd9da0afd2a3b26e1135909da5
LanceloT7730 <ac2952602@akfauniversity.org>
2022-04-28 10:55:09 +0500

    reader files are added

---
+++ reader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by doniy on 28/04/2022.
+//
+
+#include "reader.h"
---
+++ reader.h
@@ -0,0 +1,8 @@
+//
+// Created by doniy on 28/04/2022.
+//
+
+#ifndef AU_TIMETABLE_READER_H
+#define AU_TIMETABLE_READER_H
+
+#endif //AU_TIMETABLE_READER_H
